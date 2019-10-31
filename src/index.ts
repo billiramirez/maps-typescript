6dfb55275703eb3f33bc3419b6a21c772e3cd9f7
Billi Ramirez <ramirezbilly42@outlook.com>
2019-10-31 14:36:42 -0600

    marker

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import { User } from "./User";
-import { Company } from "./Company";
+
+import { CustomMap } from "./CustomMap";
 
 const user = new User();
-const company = new Company();
+const customMap = new CustomMap("map");
 
-console.log(user);
-console.log(company);
-
-// AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU
+customMap.addUserMaker(user);
